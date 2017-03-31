7264b3796da68d67bd93c9f1dc8594ba31b0f4ac
DeadUser <egor.domozhirov@gmail.com>
2017-04-01 01:12:48 +0300

    create prj

---
+++ Gomoku/Gomoku.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Gomoku
+{
+	public class Gomoku : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		public Gomoku()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+
+		protected override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+		}
+
+		protected override void UnloadContent() { }
+
+		protected override void Update(GameTime gameTime)
+		{
+			var state = Keyboard.GetState();
+			if (state.IsKeyDown(Keys.Escape)) Exit();
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+			spriteBatch.Begin();
+
+
+			spriteBatch.End();
+			base.Draw(gameTime);
+		}
+	}
+}
